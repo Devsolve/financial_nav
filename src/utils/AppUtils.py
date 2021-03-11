79cb2d9763dc546790813fe09b1b5a1872f64850
Devsolve <naskararghya620@gmail.com>
2021-03-11 17:40:11 +0530

    database change and save data of company_info

--- src/utils/AppUtils.py
+++ src/utils/AppUtils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def get_np_array(arr_len,value,data_type):
-    arr = np.empty(arr_len,dtype=data_type)
+
+def get_np_array(arr_len, value, data_type):
+    arr = np.empty(arr_len, dtype=data_type)
     arr.fill(value)
     return arr
