eedea0667be968759b333e9fd6aaa8233f2cc700
Devsolve <naskararghya620@gmail.com>
2021-03-11 12:28:29 +0530

    database change and save data of company_info

--- src/model/db_tables.py
+++ src/model/db_tables.py
@@ -22,63 +22,61 @@ dc = DbConfig()
 Base = declarative_base()
 
 
-class nav_details( Base ):
+class nav_details(Base):
     __tablename__ = 'nav_details'
-    nav_data_id = Column( INTEGER, primary_key=True )
-    tr_date = Column( DATE, nullable=False )
-    sch_code = Column( String( 50 ) )
-    mf_name = Column( String( 200 ) )
-    sch_name = Column( String( 250 ) )
-    isin_payout = Column( String( 50 ) )
-    isin_reinv = Column( String( 50 ) )
-    nav_value = Column( FLOAT, nullable=False )
-    purchase_amt = Column( FLOAT )
-    sell_amt = Column( FLOAT )
-    fund_status_type = Column( String( 200 ) )
-    scheme_type = Column( String( 200 ) )
-    fund_type = Column( String( 200 ) )
+    nav_data_id = Column(INTEGER, primary_key=True)
+    tr_date = Column(DATE, nullable=False)
+    sch_code = Column(String(50))
+    mf_name = Column(String(200))
+    sch_name = Column(String(250))
+    isin_payout = Column(String(50))
+    isin_reinv = Column(String(50))
+    nav_value = Column(FLOAT, nullable=False)
+    purchase_amt = Column(FLOAT)
+    sell_amt = Column(FLOAT)
+    fund_status_type = Column(String(200))
+    scheme_type = Column(String(200))
+    fund_type = Column(String(200))
 
 
-class scheme_detail( Base ):
+class scheme_detail(Base):
     __tablename__ = 'scheme_detail'
-    scheme_code = Column(String( 50 ), primary_key=True)
-    company_id = Column( INTEGER, nullable=False )
-    scheme_name = Column( String(250), nullable=False,unique=True )
+    scheme_code = Column(String(50), primary_key=True)
+    company_id = Column(INTEGER, nullable=False)
+    scheme_name = Column(String(250), nullable=False, unique=True)
     isin = Column(String(30))
-    scheme_description = Column( String )
-    added_on = Column( DATETIME )
-    is_active = Column( String, nullable=False )
-    updated_on = Column( DATETIME )
+    scheme_description = Column(String)
+    added_on = Column(DATETIME)
+    is_active = Column(String, nullable=False)
+    updated_on = Column(DATETIME)
 
 
-class scheme_type( Base ):
+class scheme_type(Base):
     __tablename__ = 'scheme_type'
-    sch_type_id = Column( INTEGER, primary_key=True )
-    sch_type_short_name = Column( String, nullable=False )
-    sch_type_name = Column( String, nullable=False )
-    sch_type_description = Column( String )
+    sch_type_id = Column(INTEGER, primary_key=True)
+    sch_type_short_name = Column(String, nullable=False)
+    sch_type_name = Column(String, nullable=False)
+    sch_type_description = Column(String)
 
 
-class company_info( Base ):
+class company_info(Base):
     __tablename__ = 'company_info'
-    company_id = Column( INTEGER, primary_key=True )
-    company_name = Column( String, nullable=False , unique=True)
-    company_description = Column( String )
-    added_on = Column( DATETIME )
+    company_id = Column(INTEGER, primary_key=True)
+    company_name = Column(String, nullable=False, unique=True)
+    company_description = Column(String)
+    added_on = Column(DATETIME)
 
 
-class daily_nav( Base ):
+class daily_nav(Base):
     __tablename__ = 'daily_nav'
-    nav_id = Column( INTEGER, primary_key=True )
-    scheme_code = Column( String( 50 ))
-    sch_type_id = Column( INTEGER )
-    nav_value = Column( FLOAT, nullable=False )
-    carry_forward = Column( String )
-    purchase_amt = Column( FLOAT )
-    sell_amt = Column( FLOAT )
-    added_on = Column( DATETIME )
-
-
+    nav_id = Column(INTEGER, primary_key=True)
+    scheme_code = Column(String(50))
+    sch_type_id = Column(INTEGER)
+    nav_value = Column(FLOAT, nullable=False)
+    carry_forward = Column(String)
+    purchase_amt = Column(FLOAT)
+    sell_amt = Column(FLOAT)
+    added_on = Column(DATETIME)
 
 # Base.metadata.drop_all( dc.get_engine() )
 # Base.metadata.create_all( dc.get_engine() )
--- src/repository/master_data_repository.py
+++ src/repository/master_data_repository.py
@@ -8,14 +8,14 @@ from src.model.db_tables import company_info
 
 
 def insert_company_info():
-    print( 'insert_company_info' )
+    print('insert_company_info')
     dc = DbConfig()
     con = dc.get_engine()
     sql = '''select distinct mf_name as company_name from nav_details'''
     nav_company_names_df = pd.read_sql_query(sql=sql, con=con, params=None)
     existing_company_sql = '''select distinct company_name from company_info'''
     existing_company_names_df = pd.read_sql_query(sql=existing_company_sql, con=con, params=None)
-    new_company_names_df = pd.concat([nav_company_names_df,existing_company_names_df])
+    new_company_names_df = pd.concat([nav_company_names_df, existing_company_names_df])
     new_company_names_df = new_company_names_df.drop_duplicates(keep=False)
     new_company_names_df.to_sql('company_info', con=con, if_exists='append', chunksize=1000, index=False)
 
@@ -36,32 +36,32 @@ def insert_company_info():
 
 
 def insert_daily_nav():
-    print( 'insert_daily_nav' )
+    print('insert_daily_nav')
     dc = DbConfig()
     con = dc.get_engine()
     company_sql = 'select company_id,company_name from company_info'
-    company_df = pd.read_sql_query( company_sql, con )
+    company_df = pd.read_sql_query(company_sql, con)
     comp_nm = 'Baroda Mutual Fund'
-    company_id = get_company_id( company_df, comp_nm )
-    print( company_id )
+    company_id = get_company_id(company_df, comp_nm)
+    print(company_id)
 
     scheme_detail_sql = '''select scheme_code_id,scheme_name from scheme_detail where is_active='Y' '''
-    scheme_detail_df = pd.read_sql_query( scheme_detail_sql, con )
+    scheme_detail_df = pd.read_sql_query(scheme_detail_sql, con)
     sch_name = 'ICICI Prudential Long Term Equity Fund (Tax Saving) - Growth Payout'
-    scheme_code_id = get_scheme_code_id( scheme_detail_df, sch_name )
-    print( scheme_code_id )
+    scheme_code_id = get_scheme_code_id(scheme_detail_df, sch_name)
+    print(scheme_code_id)
 
     scheme_type_sql = 'select sch_type_id,sch_type_name from scheme_type'
-    scheme_type_df = pd.read_sql_query( scheme_type_sql, con )
+    scheme_type_df = pd.read_sql_query(scheme_type_sql, con)
     sch_typ_nam = 'Debt Scheme'
-    sch_type_id = get_sch_type_id( scheme_type_df, sch_typ_nam )
-    print( sch_type_id )
+    sch_type_id = get_sch_type_id(scheme_type_df, sch_typ_nam)
+    print(sch_type_id)
 
     tr_dt = '01-Apr-2006'
     nav_detail_sql = '''select sch_name as scheme_name,scheme_type as sch_type_name, isin_payout , isin_reinv , 
     nav_value ,purchase_amt ,sell_amt , tr_date as added_on from nav_details where tr_date =:tr_date '''
-    nav_detail_df = pd.read_sql_query( sql=nav_detail_sql, con=con, params={'tr_date': tr_dt} )
-    print( nav_detail_df )
+    nav_detail_df = pd.read_sql_query(sql=nav_detail_sql, con=con, params={'tr_date': tr_dt})
+    print(nav_detail_df)
 
 
 def get_company_id(company_df, comp_nm):
@@ -77,8 +77,6 @@ def get_scheme_code_id(scheme_detail_df, sch_name):
 def get_sch_type_id(scheme_type_df, sch_typ_nam):
     sch_type_id = scheme_type_df.loc[scheme_type_df.sch_type_name == sch_typ_nam, 'sch_type_id'].values[0]
     return sch_type_id
-
-
 
 # insert_daily_nav()
 # insert_company_info()
